3ac2e094fce8ce9d69ee34d0508ffd76418715dc
fhira21 <linlinmeilani21@gmail.com>
2024-07-01 20:29:39 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## PRAKTIKUM TEKNOLOGI MOBILE PRAK KE 8
+Nama :
+
+Fhira Triana Maulani
+
+Nim :
+
+2100016012
+
+Kelas :
+
+Bahasa Inggris
+
+=======
---
+++ lib/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const kShrineBlue50 = Color.fromARGB(255, 55, 167, 227);
+const kShrineBlue100 = Color.fromARGB(255, 46, 88, 194);
+const kShrineBlue300 = Color.fromARGB(255, 22, 90, 235);
+const kShrineBlue400 = Color.fromARGB(255, 47, 66, 156);
+
+const kShrineBrown900 = Color(0xFF442B2D);
+
+const kShrineErrorRed = Color(0xFFC5032B);
+
+const kShrineSurfaceWhite = Color(0xFFFFFBFA);
+const kShrineBackgroundWhite = Colors.white;
+
+const kShrinePurple = Color(0xFF5D1049);
